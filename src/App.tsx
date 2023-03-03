ce1bdac25729aa593e94e349b42940dd8e56c02e
Abu Raihan <raihanahmed01973@gmail.com>
2023-03-03 11:04:52 +0600

    array using

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import User from "./Folder/User";
 
 function App() {
   return (
     <div className="App">
-      <User name="raihan" age={20} isAdult={true}></User>
+      <User
+        name="raihan"
+        age={20}
+        isAdult={true}
+        lang={["bangla", "english", "hindi"]}
+      ></User>
     </div>
   );
 }
--- src/Folder/User.tsx
+++ src/Folder/User.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-type userProps = { name: string; age: number; isAdult: boolean };
-const User = ({ name, age, isAdult }: userProps) => {
+type userProps = {
+  name: string;
+  age: number;
+  isAdult: boolean;
+  lang: string[];
+};
+const User = ({ name, age, isAdult, lang }: userProps) => {
   return (
     <div>
       <h2>{name}</h2>
       <p>{age} years old</p>
       <p>{isAdult ? "he is a adult" : "he is not a adult"}</p>
+      <p>
+        {lang.map((language, idx) => {
+          return <span key={idx}>{language} </span>;
+        })}
+      </p>
     </div>
   );
 };
